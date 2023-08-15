69e28e1fcdcd3b2c3e791a6d32fd086ddb376df1
jhonatanGuingo <jhonatan.amaral@engenharia.ufjf.br>
2023-08-14 21:31:26 -0300

    initial commit

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import {Router} from "express"
+import dotenv from "dotenv"
+import express from "express"
+import cors from "cors"
+import router from "./routes/router.js";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(router);
+
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+	console.log(`Servidor rodando na porta ${port}`)
+})
