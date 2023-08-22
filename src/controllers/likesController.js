a1a538d0a5d286e44186869fed7dc4275d0f3ee9
DanielCamposDevX <daniel.campos.dev3@gmail.com>
2023-08-22 11:26:01 -0300

    Likes Logic

--- src/controllers/likesController.js
+++ src/controllers/likesController.js
@@ -1,21 +1,34 @@
-import { getlikeNumber, liked } from "../repositories/likesRepositorie.js";
+import { getlikeNumber, handleLike, liked } from "../repositories/likesRepositorie.js";
 
-export async function getLikes(req,res){
+export async function getLikes(req, res) {
   const { postid, userid } = req.params;
-  console.log(postid)
-  if(!userid || !postid ){return res.status(409).send('missing ids')}
-    try{
-    const likes = await getlikeNumber(postid);
-    if(likes.err){return res.status(500).send(likes.err)};
 
-    const likedu = await liked(postid,userid);
-    if(likedu.err){return res.status(500).send(likedu.err)};
+  if (!userid || !postid) { return res.status(409).send('missing ids') }
+  try {
+    const likesNumber = await getlikeNumber(postid);
+    if (likesNumber.err) { return res.status(500).send(likesNumber.err) };
 
-    return res.status(200).send({likes,likedu})
-    }
-    catch(err){
-        return res.status(500).send(err)
-    }
+    const likeUsers = await liked(postid, userid);
+    if (likeUsers.err) { return res.status(500).send(likeUsers.err) };
 
+    return res.status(200).send({ likesNumber, likeUsers })
+  }
+  catch (err) {
+    return res.status(500).send(err)
+  }
+}
+
+
+export async function handleLikePost(req, res) {
+  const { postid, userid, bool } = req.body;
+
+  try{
+    const like = handleLike(postid,userid,bool)
+    if(like.err){return res.status(500).send(like.err);}
+    return res.sendStatus(200);
+  }
+  catch(err){
+    return res.status(500).send(err);
+  }
 }
 
